9e6aa7650531d8f22965fa0635346a3d42dbc1f1
rasambowl <ezioflame64@gmail.com>
2019-10-22 12:39:57 -0400

    Getting Sample Data Info

--- dataTest.py
+++ dataTest.py
@@ -10,4 +10,13 @@ level5data.render_sample(firstsampletoken)
 first = level5data.get('sample', firstsampletoken)
 next = level5data.get('sample', first['next'])
 level5data.render_sample(next['token'])
-timediff = next['timestamp']-first['timestamp']
+timediff = next['timestamp']-first['timestamp'] #should show time difference, not working right now, needed for displacement calc
+
+sensor_channel = 'LIDAR_TOP'  # also try this e.g. with 'LIDAR_TOP'
+my_sample_data = level5data.get('sample_data', first['data'][sensor_channel])
+
+my_annotation_token = first['anns'][5]
+my_annotation =  my_sample_data.get('sample_annotation', my_annotation_token)
+level5data.render_annotation(my_annotation_token)
+
+#LOOK AT ANNOTATION STUFF TO GET COORDINATES AND OTHER STUFF
